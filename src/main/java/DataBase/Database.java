060ae547742a6c0cce7e874a252acdc935ca9ff1
Terence WODLING <terencewodling@gmail.com>
2018-11-24 09:24:49 +0100

    Fichiers Magasins

---
+++ src/main/java/DataBase/Database.java
@@ -0,0 +1,39 @@
+package DataBase;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+
+	/*Integration : 10.10.2.2*/
+		 private static final String URL = "jdbc:mysql://localhost:3306/databaseSql";// mysql 3306 postgre 5432 // changer ICI pour bdd
+		 private static final String USER = "app";// fonctionne pas	
+		 private static final String PASSWORD = "1234"; // fonctionne pas
+		 private static final String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
+		 
+		 static {
+			 try {
+				 Class.forName(DRIVER_NAME).newInstance();
+				 System.out.println("* Driver loaded.");
+			 }
+			 catch (ClassNotFoundException e) {
+				 System.err.println("* ERROR: Driver " + DRIVER_NAME + "not found");
+			 }
+			 catch (InstantiationException e) {
+				 System.err.println("* ERROR: Impossible to create aninstance of " + DRIVER_NAME);
+				 System.err.println(e.getMessage());
+			 }
+			 catch (IllegalAccessException e) {
+				 System.err.println("* ERROR: Impossible to create aninstance of " + DRIVER_NAME);
+				 System.err.println(e.getMessage());
+			 }
+		 }
+		 
+		public static Connection getConnection() throws SQLException {
+			 return DriverManager.getConnection(URL, USER, PASSWORD);
+		}
+		 
+	}
+
+
---
+++ src/test/java/trueTest/AppTest2.java
@@ -0,0 +1,14 @@
+package trueTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest2 {
+
+	@Test
+	public void test() {
+		assertTrue(false);
+	}
+
+}
