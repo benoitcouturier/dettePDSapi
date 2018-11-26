8fe77f36b3cbe311854736536a1913f20d012184
Oufini Roua <roua.oufini@etu.u-pec.fr>
2018-11-26 18:23:16 +0100

    DAO Roua Client

---
+++ src/main/java/DAO/portail/gererSonCompteClient/ClientAccountDAO.java
@@ -0,0 +1,7 @@
+package DAO.portail.gererSonCompteClient;
+
+import DAO.CrudDAO;
+
+public interface ClientAccountDAO<T> extends CrudDAO<T> {
+
+}
---
+++ src/main/java/DAO/portail/gererSonCompteClient/ClientAccountDAOImpl.java
@@ -0,0 +1,68 @@
+package DAO.portail.gererSonCompteClient;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import DAO.portail.gererSonCompteClient.ClientAccountDAO;
+import DataBase.Database;
+import Entites.portail.gererSonCompteClient.Customer_account;
+
+public class ClientAccountDAOImpl implements ClientAccountDAO<Customer_account> {
+
+
+	@Override
+	public void create(Customer_account object) throws Exception {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+		Connection conn;
+		PreparedStatement state;
+		
+		try{
+			int i =1;
+			conn = Database.getConnection();
+			String sql = "INSERT INTO customer_account VALUES(?,?,?,?,?,?,?,?)";
+			state = conn.prepareStatement(sql);
+			state.setInt(i++,object.getCustomer_phone());
+			state.setString(i++, object.getAdress());
+			state.setString(i++, object.getCity());
+			state.setString(i++, object.getState());
+			state.setString(i++, object.getEmail());
+			state.setString(i++, object.getSex());
+			state.setInt(i++, object.getAge());
+		} catch(SQLException e){
+			e.printStackTrace();
+		}
+
+		
+	}
+
+	@Override
+	public void delete(Customer_account object) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Customer_account object, int id) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Customer_account find(Customer_account object) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+	@Override
+	public ArrayList<Customer_account> read() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+}
