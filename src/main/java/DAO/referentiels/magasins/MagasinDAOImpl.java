ddf3e91556504be7d897765959541478dfeb786c
Terence WODLING <terencewodling@gmail.com>
2018-12-01 01:07:11 +0100

    crud UC1

--- src/main/java/DAO/referentiels/magasins/MagasinDAOImpl.java
+++ src/main/java/DAO/referentiels/magasins/MagasinDAOImpl.java
@@ -89,218 +89,218 @@ public class MagasinDAOImpl implements MagasinsDAO<Magasin> {
 		try {
 			int i =1;
 			connect = Database.getConnection();
-			String sql = "Drop table Emplacements;\n" + 
-					"Drop table TypeMagasin;\n" + 
-					"Drop table Magasins;\n" + 
-					"Drop table EmplacementsMagasins;\n" + 
-					"\n" + 
-					"CREATE TABLE Emplacements (\n" + 
-					"id INT PRIMARY KEY AUTO_INCREMENT,\n" + 
-					"superficie INT,\n" + 
-					"aile VARCHAR(50),\n" + 
-					"importance INT\n" + 
-					");\n" + 
-					"\n" + 
-					"INSERT INTO `Emplacements` (`superficie`,`aile`,`importance`) VALUES \n" + 
-					"(213,\"Purus Gravida Ltd\",5),\n" + 
-					"(351,\"Lorem Donec Elementum Corp.\",6),\n" + 
-					"(315,\"Ac Turpis Corporation\",8),\n" + 
-					"(227,\"Leo Corporation\",1),\n" + 
-					"(345,\"Dapibus Ligula Industries\",8),\n" + 
-					"(259,\"Ipsum Leo Elementum Ltd\",10),\n" + 
-					"(236,\"Laoreet Ltd\",3),\n" + 
-					"(143,\"Dis Parturient Limited\",9),\n" + 
-					"(163,\"Nulla Tempor Institute\",7),\n" + 
-					"(184,\"Quisque Fringilla Foundation\",6);\n" + 
-					"\n" + 
-					"INSERT INTO `Emplacements` (`superficie`,`aile`,`importance`) VALUES \n" + 
-					"(108,\"Placerat Corp.\",4),\n" + 
-					"(113,\"Duis Consulting\",3),\n" + 
-					"(118,\"Turpis Corporation\",2),\n" + 
-					"(126,\"Eleifend Nunc Foundation\",6),\n" + 
-					"(313,\"Vel Lectus PC\",6),\n" + 
-					"(176,\"Purus Corporation\",10),\n" + 
-					"(188,\"Libero Consulting\",5),\n" + 
-					"(221,\"Sed Dictum Industries\",2),\n" + 
-					"(248,\"Luctus Ut LLP\",9),\n" + 
-					"(373,\"Sociis Inc.\",6);\n" + 
-					"\n" + 
-					"INSERT INTO `Emplacements` (`superficie`,`aile`,`importance`) VALUES \n" + 
-					"(291,\"Vestibulum PC\",9),\n" + 
-					"(250,\"Sagittis Lobortis Limited\",8),\n" + 
-					"(205,\"Lacus Nulla LLC\",9),\n" + 
-					"(394,\"Arcu Sed Incorporated\",2),\n" + 
-					"(208,\"Quisque Imperdiet Industries\",10),\n" + 
-					"(206,\"Nec Tempus Mauris Consulting\",3),\n" + 
-					"(247,\"Et Netus Et Corp.\",5),\n" + 
-					"(144,\"Dictum Eleifend Company\",3),\n" + 
-					"(318,\"Eu Tellus Eu Associates\",10),\n" + 
-					"(181,\"Non Dui Nec Corp.\",3);\n" + 
-					"\n" + 
-					"INSERT INTO `Emplacements` (`superficie`,`aile`,`importance`) VALUES \n" + 
-					"(163,\"Sit Amet Consectetuer PC\",8),\n" + 
-					"(162,\"Natoque Ltd\",3),\n" + 
-					"(358,\"Litora Torquent Inc.\",5),\n" + 
-					"(193,\"Et Consulting\",3),\n" + 
-					"(361,\"Augue Ut Lacus Institute\",9),\n" + 
-					"(175,\"Cursus Foundation\",4),\n" + 
-					"(320,\"Eu Lacus Ltd\",7),\n" + 
-					"(367,\"Dui Lectus Consulting\",5),\n" + 
-					"(308,\"Ligula Aenean Foundation\",8),\n" + 
-					"(272,\"Nunc LLC\",10);\n" + 
-					"\n" + 
-					"INSERT INTO `Emplacements` (`superficie`,`aile`,`importance`) VALUES \n" + 
-					"(266,\"Parturient Industries\",9),\n" + 
-					"(118,\"Fusce Aliquet Magna Industries\",4),\n" + 
-					"(105,\"Sit Amet Risus LLP\",8),\n" + 
-					"(181,\"Sem Institute\",8),\n" + 
-					"(382,\"Mauris Non Ltd\",10),\n" + 
-					"(359,\"Tincidunt Donec Vitae Institute\",7),\n" + 
-					"(376,\"Luctus Aliquet Odio LLC\",1),\n" + 
-					"(392,\"Nec LLC\",4),\n" + 
-					"(203,\"Ante Lectus Incorporated\",7),\n" + 
-					"(214,\"Augue Eu Tempor LLC\",3);\n" + 
-					"\n" + 
-					"INSERT INTO `Emplacements` (`superficie`,`aile`,`importance`) VALUES\n" + 
-					"(157,\"Commodo Auctor Velit LLC\",1),\n" + 
-					"(353,\"Arcu Ac Institute\",5),\n" + 
-					"(188,\"Adipiscing Lacus Corporation\",9),\n" + 
-					"(215,\"Interdum Sed Consulting\",6),\n" + 
-					"(280,\"Integer Foundation\",9),\n" + 
-					"(228,\"A Feugiat Tellus Consulting\",5),\n" + 
-					"(262,\"Sem Limited\",9),\n" + 
-					"(162,\"Proin Ltd\",7),\n" + 
-					"(271,\"Turpis Aliquam Company\",8),\n" + 
-					"(274,\"Non Lacinia Foundation\",10);\n" + 
-					"\n" + 
-					"INSERT INTO `Emplacements` (`superficie`,`aile`,`importance`) VALUES \n" + 
-					"(162,\"Cras Pellentesque PC\",4),\n" + 
-					"(312,\"Velit Corporation\",2),\n" + 
-					"(272,\"Cum Sociis Corporation\",1),\n" + 
-					"(156,\"Massa Company\",2),\n" + 
-					"(173,\"Feugiat Nec Diam Limited\",1),\n" + 
-					"(309,\"Ullamcorper Inc.\",8),\n" + 
-					"(371,\"Sapien Cursus LLC\",5),\n" + 
-					"(268,\"Arcu Limited\",4),\n" + 
-					"(327,\"Duis Limited\",7),\n" + 
-					"(187,\"Blandit LLP\",3);\n" + 
-					"\n" + 
-					"INSERT INTO `Emplacements` (`superficie`,`aile`,`importance`) VALUES\n" + 
-					"(388,\"Metus Aenean Ltd\",10),\n" + 
-					"(242,\"Et Inc.\",5),\n" + 
-					"(298,\"Fringilla Purus Corp.\",6),\n" + 
-					"(179,\"Aliquet Corp.\",8),\n" + 
-					"(299,\"Commodo Ipsum Foundation\",10),\n" + 
-					"(135,\"Adipiscing Ltd\",2),\n" + 
-					"(189,\"Arcu Sed Consulting\",9),\n" + 
-					"(373,\"Nec Ante Limited\",9),\n" + 
-					"(397,\"Aliquam LLP\",7),\n" + 
-					"(190,\"Quisque Varius Inc.\",9);\n" + 
-					"\n" + 
-					"INSERT INTO `Emplacements` (`superficie`,`aile`,`importance`) VALUES\n" + 
-					"(170,\"Id Ante PC\",9),\n" + 
-					"(214,\"Phasellus Incorporated\",2),\n" + 
-					"(155,\"Fusce Mollis Corporation\",1),\n" + 
-					"(377,\"Nec Urna Corp.\",6),\n" + 
-					"(130,\"Ut Eros Non PC\",1),\n" + 
-					"(273,\"Blandit Nam Nulla Ltd\",5),\n" + 
-					"(292,\"Eu Metus Foundation\",10),\n" + 
-					"(300,\"Tellus Incorporated\",8),\n" + 
-					"(212,\"Sodales Mauris Blandit PC\",10),\n" + 
-					"(383,\"Erat Eget Ipsum LLP\",3);\n" + 
-					"\n" + 
-					"INSERT INTO `Emplacements` (`superficie`,`aile`,`importance`) VALUES\n" + 
-					"(364,\"Donec Fringilla Donec PC\",2),\n" + 
-					"(353,\"A Aliquet Vel Corporation\",2),\n" + 
-					"(279,\"Velit LLP\",5),\n" + 
-					"(393,\"Erat Etiam Inc.\",7),\n" + 
-					"(158,\"Quisque Varius Industries\",3),\n" + 
-					"(216,\"Ornare Company\",9),\n" + 
-					"(337,\"Mauris Non Dui Foundation\",1),\n" + 
-					"(378,\"Sed Leo Industries\",1),\n" + 
-					"(142,\"Mattis Ornare LLC\",3),\n" + 
-					"(137,\"Nulla Tincidunt Neque Ltd\",4);\n" + 
-					"\n" + 
-					"INSERT INTO `Emplacements` (`superficie`,`aile`,`importance`) VALUES\n" + 
-					"(157,\"Commodo Auctor Velit LLC\",1),\n" + 
-					"(353,\"Arcu Ac Institute\",5),\n" + 
-					"(188,\"Adipiscing Lacus Corporation\",9),\n" + 
-					"(215,\"Interdum Sed Consulting\",6),\n" + 
-					"(280,\"Integer Foundation\",9),\n" + 
-					"(228,\"A Feugiat Tellus Consulting\",5),\n" + 
-					"(262,\"Sem Limited\",9),\n" + 
-					"(162,\"Proin Ltd\",7),\n" + 
-					"(271,\"Turpis Aliquam Company\",8),\n" + 
-					"(274,\"Non Lacinia Foundation\",10);\n" + 
-					"\n" + 
-					"INSERT INTO `Emplacements` (`superficie`,`aile`,`importance`) VALUES \n" + 
-					"(162,\"Cras Pellentesque PC\",4),\n" + 
-					"(312,\"Velit Corporation\",2),\n" + 
-					"(272,\"Cum Sociis Corporation\",1),\n" + 
-					"(156,\"Massa Company\",2),\n" + 
-					"(173,\"Feugiat Nec Diam Limited\",1),\n" + 
-					"(309,\"Ullamcorper Inc.\",8),\n" + 
-					"(371,\"Sapien Cursus LLC\",5),\n" + 
-					"(268,\"Arcu Limited\",4),\n" + 
-					"(327,\"Duis Limited\",7),\n" + 
-					"(187,\"Blandit LLP\",3);\n" + 
-					"\n" + 
-					"INSERT INTO `Emplacements` (`superficie`,`aile`,`importance`) VALUES\n" + 
-					"(388,\"Metus Aenean Ltd\",10),\n" + 
-					"(242,\"Et Inc.\",5),\n" + 
-					"(298,\"Fringilla Purus Corp.\",6),\n" + 
-					"(179,\"Aliquet Corp.\",8),\n" + 
-					"(299,\"Commodo Ipsum Foundation\",10),\n" + 
-					"(135,\"Adipiscing Ltd\",2),\n" + 
-					"(189,\"Arcu Sed Consulting\",9),\n" + 
-					"(373,\"Nec Ante Limited\",9),\n" + 
-					"(397,\"Aliquam LLP\",7),\n" + 
-					"(190,\"Quisque Varius Inc.\",9);\n" + 
-					"\n" + 
-					"INSERT INTO `Emplacements` (`superficie`,`aile`,`importance`) VALUES\n" + 
-					"(170,\"Id Ante PC\",9),\n" + 
-					"(214,\"Phasellus Incorporated\",2),\n" + 
-					"(155,\"Fusce Mollis Corporation\",1),\n" + 
-					"(377,\"Nec Urna Corp.\",6),\n" + 
-					"(130,\"Ut Eros Non PC\",1),\n" + 
-					"(273,\"Blandit Nam Nulla Ltd\",5),\n" + 
-					"(292,\"Eu Metus Foundation\",10),\n" + 
-					"(300,\"Tellus Incorporated\",8),\n" + 
-					"(212,\"Sodales Mauris Blandit PC\",10),\n" + 
-					"(383,\"Erat Eget Ipsum LLP\",3);\n" + 
-					"\n" + 
-					"INSERT INTO `Emplacements` (`superficie`,`aile`,`importance`) VALUES\n" + 
-					"(364,\"Donec Fringilla Donec PC\",2),\n" + 
-					"(353,\"A Aliquet Vel Corporation\",2),\n" + 
-					"(279,\"Velit LLP\",5),\n" + 
-					"(393,\"Erat Etiam Inc.\",7),\n" + 
-					"(158,\"Quisque Varius Industries\",3),\n" + 
-					"(216,\"Ornare Company\",9),\n" + 
-					"(337,\"Mauris Non Dui Foundation\",1),\n" + 
-					"(378,\"Sed Leo Industries\",1),\n" + 
-					"(142,\"Mattis Ornare LLC\",3),\n" + 
-					"(137,\"Nulla Tincidunt Neque Ltd\",4);\n" + 
-					"\n" + 
-					"CREATE TABLE TypeMagasin (\n" + 
-					"id INT PRIMARY KEY AUTO_INCREMENT,\n" + 
-					"type varchar(50)\n" + 
-					");\n" + 
-					"\n" + 
-					"CREATE TABLE Magasins (\n" + 
-					"INT id PRIMARY KEY AUTO_INCREMENT,\n" + 
-					"nom VARCHAR(50),\n" + 
-					"description VARCHAR(250),\n" + 
-					"idEmplacement int,\n" + 
-					"idType int,\n" + 
-					"FOREIGN KEY (idType) REFERENCES TypeMagasin(id)\n" + 
-					");\n" + 
-					"\n" + 
-					"CREATE TABLE EmplacementMagasin (\n" + 
-					"id INT PRIMARY KEY AUTO_INCREMENT,\n" + 
-					"idEmplacement INT,\n" + 
-					"FOREIGN KEY (idEmplacement) REFERENCES Emplacements(id)\n" + 
-					");\n" + 
-					"\n" + 
+			String sql = "Drop table Emplacements;\r\n" + 
+					"Drop table TypeMagasin;\r\n" + 
+					"Drop table Magasins;\r\n" + 
+					"Drop table EmplacementsMagasins;\r\n" + 
+					"\r\n" + 
+					"CREATE TABLE Emplacements (\r\n" + 
+					"id INT PRIMARY KEY AUTO_INCREMENT,\r\n" + 
+					"superficie INT,\r\n" + 
+					"aile VARCHAR(50),\r\n" + 
+					"importance INT\r\n" + 
+					");\r\n" + 
+					"\r\n" + 
+					"INSERT INTO `Emplacements` (`superficie`,`aile`,`importance`) VALUES \r\n" + 
+					"(213,\"Purus Gravida Ltd\",5),\r\n" + 
+					"(351,\"Lorem Donec Elementum Corp.\",6),\r\n" + 
+					"(315,\"Ac Turpis Corporation\",8),\r\n" + 
+					"(227,\"Leo Corporation\",1),\r\n" + 
+					"(345,\"Dapibus Ligula Industries\",8),\r\n" + 
+					"(259,\"Ipsum Leo Elementum Ltd\",10),\r\n" + 
+					"(236,\"Laoreet Ltd\",3),\r\n" + 
+					"(143,\"Dis Parturient Limited\",9),\r\n" + 
+					"(163,\"Nulla Tempor Institute\",7),\r\n" + 
+					"(184,\"Quisque Fringilla Foundation\",6);\r\n" + 
+					"\r\n" + 
+					"INSERT INTO `Emplacements` (`superficie`,`aile`,`importance`) VALUES \r\n" + 
+					"(108,\"Placerat Corp.\",4),\r\n" + 
+					"(113,\"Duis Consulting\",3),\r\n" + 
+					"(118,\"Turpis Corporation\",2),\r\n" + 
+					"(126,\"Eleifend Nunc Foundation\",6),\r\n" + 
+					"(313,\"Vel Lectus PC\",6),\r\n" + 
+					"(176,\"Purus Corporation\",10),\r\n" + 
+					"(188,\"Libero Consulting\",5),\r\n" + 
+					"(221,\"Sed Dictum Industries\",2),\r\n" + 
+					"(248,\"Luctus Ut LLP\",9),\r\n" + 
+					"(373,\"Sociis Inc.\",6);\r\n" + 
+					"\r\n" + 
+					"INSERT INTO `Emplacements` (`superficie`,`aile`,`importance`) VALUES \r\n" + 
+					"(291,\"Vestibulum PC\",9),\r\n" + 
+					"(250,\"Sagittis Lobortis Limited\",8),\r\n" + 
+					"(205,\"Lacus Nulla LLC\",9),\r\n" + 
+					"(394,\"Arcu Sed Incorporated\",2),\r\n" + 
+					"(208,\"Quisque Imperdiet Industries\",10),\r\n" + 
+					"(206,\"Nec Tempus Mauris Consulting\",3),\r\n" + 
+					"(247,\"Et Netus Et Corp.\",5),\r\n" + 
+					"(144,\"Dictum Eleifend Company\",3),\r\n" + 
+					"(318,\"Eu Tellus Eu Associates\",10),\r\n" + 
+					"(181,\"Non Dui Nec Corp.\",3);\r\n" + 
+					"\r\n" + 
+					"INSERT INTO `Emplacements` (`superficie`,`aile`,`importance`) VALUES \r\n" + 
+					"(163,\"Sit Amet Consectetuer PC\",8),\r\n" + 
+					"(162,\"Natoque Ltd\",3),\r\n" + 
+					"(358,\"Litora Torquent Inc.\",5),\r\n" + 
+					"(193,\"Et Consulting\",3),\r\n" + 
+					"(361,\"Augue Ut Lacus Institute\",9),\r\n" + 
+					"(175,\"Cursus Foundation\",4),\r\n" + 
+					"(320,\"Eu Lacus Ltd\",7),\r\n" + 
+					"(367,\"Dui Lectus Consulting\",5),\r\n" + 
+					"(308,\"Ligula Aenean Foundation\",8),\r\n" + 
+					"(272,\"Nunc LLC\",10);\r\n" + 
+					"\r\n" + 
+					"INSERT INTO `Emplacements` (`superficie`,`aile`,`importance`) VALUES \r\n" + 
+					"(266,\"Parturient Industries\",9),\r\n" + 
+					"(118,\"Fusce Aliquet Magna Industries\",4),\r\n" + 
+					"(105,\"Sit Amet Risus LLP\",8),\r\n" + 
+					"(181,\"Sem Institute\",8),\r\n" + 
+					"(382,\"Mauris Non Ltd\",10),\r\n" + 
+					"(359,\"Tincidunt Donec Vitae Institute\",7),\r\n" + 
+					"(376,\"Luctus Aliquet Odio LLC\",1),\r\n" + 
+					"(392,\"Nec LLC\",4),\r\n" + 
+					"(203,\"Ante Lectus Incorporated\",7),\r\n" + 
+					"(214,\"Augue Eu Tempor LLC\",3);\r\n" + 
+					"\r\n" + 
+					"INSERT INTO `Emplacements` (`superficie`,`aile`,`importance`) VALUES\r\n" + 
+					"(157,\"Commodo Auctor Velit LLC\",1),\r\n" + 
+					"(353,\"Arcu Ac Institute\",5),\r\n" + 
+					"(188,\"Adipiscing Lacus Corporation\",9),\r\n" + 
+					"(215,\"Interdum Sed Consulting\",6),\r\n" + 
+					"(280,\"Integer Foundation\",9),\r\n" + 
+					"(228,\"A Feugiat Tellus Consulting\",5),\r\n" + 
+					"(262,\"Sem Limited\",9),\r\n" + 
+					"(162,\"Proin Ltd\",7),\r\n" + 
+					"(271,\"Turpis Aliquam Company\",8),\r\n" + 
+					"(274,\"Non Lacinia Foundation\",10);\r\n" + 
+					"\r\n" + 
+					"INSERT INTO `Emplacements` (`superficie`,`aile`,`importance`) VALUES \r\n" + 
+					"(162,\"Cras Pellentesque PC\",4),\r\n" + 
+					"(312,\"Velit Corporation\",2),\r\n" + 
+					"(272,\"Cum Sociis Corporation\",1),\r\n" + 
+					"(156,\"Massa Company\",2),\r\n" + 
+					"(173,\"Feugiat Nec Diam Limited\",1),\r\n" + 
+					"(309,\"Ullamcorper Inc.\",8),\r\n" + 
+					"(371,\"Sapien Cursus LLC\",5),\r\n" + 
+					"(268,\"Arcu Limited\",4),\r\n" + 
+					"(327,\"Duis Limited\",7),\r\n" + 
+					"(187,\"Blandit LLP\",3);\r\n" + 
+					"\r\n" + 
+					"INSERT INTO `Emplacements` (`superficie`,`aile`,`importance`) VALUES\r\n" + 
+					"(388,\"Metus Aenean Ltd\",10),\r\n" + 
+					"(242,\"Et Inc.\",5),\r\n" + 
+					"(298,\"Fringilla Purus Corp.\",6),\r\n" + 
+					"(179,\"Aliquet Corp.\",8),\r\n" + 
+					"(299,\"Commodo Ipsum Foundation\",10),\r\n" + 
+					"(135,\"Adipiscing Ltd\",2),\r\n" + 
+					"(189,\"Arcu Sed Consulting\",9),\r\n" + 
+					"(373,\"Nec Ante Limited\",9),\r\n" + 
+					"(397,\"Aliquam LLP\",7),\r\n" + 
+					"(190,\"Quisque Varius Inc.\",9);\r\n" + 
+					"\r\n" + 
+					"INSERT INTO `Emplacements` (`superficie`,`aile`,`importance`) VALUES\r\n" + 
+					"(170,\"Id Ante PC\",9),\r\n" + 
+					"(214,\"Phasellus Incorporated\",2),\r\n" + 
+					"(155,\"Fusce Mollis Corporation\",1),\r\n" + 
+					"(377,\"Nec Urna Corp.\",6),\r\n" + 
+					"(130,\"Ut Eros Non PC\",1),\r\n" + 
+					"(273,\"Blandit Nam Nulla Ltd\",5),\r\n" + 
+					"(292,\"Eu Metus Foundation\",10),\r\n" + 
+					"(300,\"Tellus Incorporated\",8),\r\n" + 
+					"(212,\"Sodales Mauris Blandit PC\",10),\r\n" + 
+					"(383,\"Erat Eget Ipsum LLP\",3);\r\n" + 
+					"\r\n" + 
+					"INSERT INTO `Emplacements` (`superficie`,`aile`,`importance`) VALUES\r\n" + 
+					"(364,\"Donec Fringilla Donec PC\",2),\r\n" + 
+					"(353,\"A Aliquet Vel Corporation\",2),\r\n" + 
+					"(279,\"Velit LLP\",5),\r\n" + 
+					"(393,\"Erat Etiam Inc.\",7),\r\n" + 
+					"(158,\"Quisque Varius Industries\",3),\r\n" + 
+					"(216,\"Ornare Company\",9),\r\n" + 
+					"(337,\"Mauris Non Dui Foundation\",1),\r\n" + 
+					"(378,\"Sed Leo Industries\",1),\r\n" + 
+					"(142,\"Mattis Ornare LLC\",3),\r\n" + 
+					"(137,\"Nulla Tincidunt Neque Ltd\",4);\r\n" + 
+					"\r\n" + 
+					"INSERT INTO `Emplacements` (`superficie`,`aile`,`importance`) VALUES\r\n" + 
+					"(157,\"Commodo Auctor Velit LLC\",1),\r\n" + 
+					"(353,\"Arcu Ac Institute\",5),\r\n" + 
+					"(188,\"Adipiscing Lacus Corporation\",9),\r\n" + 
+					"(215,\"Interdum Sed Consulting\",6),\r\n" + 
+					"(280,\"Integer Foundation\",9),\r\n" + 
+					"(228,\"A Feugiat Tellus Consulting\",5),\r\n" + 
+					"(262,\"Sem Limited\",9),\r\n" + 
+					"(162,\"Proin Ltd\",7),\r\n" + 
+					"(271,\"Turpis Aliquam Company\",8),\r\n" + 
+					"(274,\"Non Lacinia Foundation\",10);\r\n" + 
+					"\r\n" + 
+					"INSERT INTO `Emplacements` (`superficie`,`aile`,`importance`) VALUES \r\n" + 
+					"(162,\"Cras Pellentesque PC\",4),\r\n" + 
+					"(312,\"Velit Corporation\",2),\r\n" + 
+					"(272,\"Cum Sociis Corporation\",1),\r\n" + 
+					"(156,\"Massa Company\",2),\r\n" + 
+					"(173,\"Feugiat Nec Diam Limited\",1),\r\n" + 
+					"(309,\"Ullamcorper Inc.\",8),\r\n" + 
+					"(371,\"Sapien Cursus LLC\",5),\r\n" + 
+					"(268,\"Arcu Limited\",4),\r\n" + 
+					"(327,\"Duis Limited\",7),\r\n" + 
+					"(187,\"Blandit LLP\",3);\r\n" + 
+					"\r\n" + 
+					"INSERT INTO `Emplacements` (`superficie`,`aile`,`importance`) VALUES\r\n" + 
+					"(388,\"Metus Aenean Ltd\",10),\r\n" + 
+					"(242,\"Et Inc.\",5),\r\n" + 
+					"(298,\"Fringilla Purus Corp.\",6),\r\n" + 
+					"(179,\"Aliquet Corp.\",8),\r\n" + 
+					"(299,\"Commodo Ipsum Foundation\",10),\r\n" + 
+					"(135,\"Adipiscing Ltd\",2),\r\n" + 
+					"(189,\"Arcu Sed Consulting\",9),\r\n" + 
+					"(373,\"Nec Ante Limited\",9),\r\n" + 
+					"(397,\"Aliquam LLP\",7),\r\n" + 
+					"(190,\"Quisque Varius Inc.\",9);\r\n" + 
+					"\r\n" + 
+					"INSERT INTO `Emplacements` (`superficie`,`aile`,`importance`) VALUES\r\n" + 
+					"(170,\"Id Ante PC\",9),\r\n" + 
+					"(214,\"Phasellus Incorporated\",2),\r\n" + 
+					"(155,\"Fusce Mollis Corporation\",1),\r\n" + 
+					"(377,\"Nec Urna Corp.\",6),\r\n" + 
+					"(130,\"Ut Eros Non PC\",1),\r\n" + 
+					"(273,\"Blandit Nam Nulla Ltd\",5),\r\n" + 
+					"(292,\"Eu Metus Foundation\",10),\r\n" + 
+					"(300,\"Tellus Incorporated\",8),\r\n" + 
+					"(212,\"Sodales Mauris Blandit PC\",10),\r\n" + 
+					"(383,\"Erat Eget Ipsum LLP\",3);\r\n" + 
+					"\r\n" + 
+					"INSERT INTO `Emplacements` (`superficie`,`aile`,`importance`) VALUES\r\n" + 
+					"(364,\"Donec Fringilla Donec PC\",2),\r\n" + 
+					"(353,\"A Aliquet Vel Corporation\",2),\r\n" + 
+					"(279,\"Velit LLP\",5),\r\n" + 
+					"(393,\"Erat Etiam Inc.\",7),\r\n" + 
+					"(158,\"Quisque Varius Industries\",3),\r\n" + 
+					"(216,\"Ornare Company\",9),\r\n" + 
+					"(337,\"Mauris Non Dui Foundation\",1),\r\n" + 
+					"(378,\"Sed Leo Industries\",1),\r\n" + 
+					"(142,\"Mattis Ornare LLC\",3),\r\n" + 
+					"(137,\"Nulla Tincidunt Neque Ltd\",4);\r\n" + 
+					"\r\n" + 
+					"CREATE TABLE TypeMagasin (\r\n" + 
+					"id INT PRIMARY KEY AUTO_INCREMENT,\r\n" + 
+					"type varchar(50)\r\n" + 
+					");\r\n" + 
+					"\r\n" + 
+					"CREATE TABLE Magasins (\r\n" + 
+					"INT id PRIMARY KEY AUTO_INCREMENT,\r\n" + 
+					"nom VARCHAR(50),\r\n" + 
+					"description VARCHAR(250),\r\n" + 
+					"idEmplacement int,\r\n" + 
+					"idType int,\r\n" + 
+					"FOREIGN KEY (idType) REFERENCES TypeMagasin(id)\r\n" + 
+					");\r\n" + 
+					"\r\n" + 
+					"CREATE TABLE EmplacementMagasin (\r\n" + 
+					"id INT PRIMARY KEY AUTO_INCREMENT,\r\n" + 
+					"idEmplacement INT,\r\n" + 
+					"FOREIGN KEY (idEmplacement) REFERENCES Emplacements(id)\r\n" + 
+					");\r\n" + 
+					"\r\n" + 
 					"";
 			
 			ps = connect.prepareStatement(sql);
