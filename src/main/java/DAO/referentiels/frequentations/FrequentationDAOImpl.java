caeaf80be33df5b63f7fc27248adf83fd91e7fcf
Thomas <tbringtown@hotmail.fr>
2019-03-18 14:16:41 +0100

    Mocks avec dates aleatoires

--- src/main/java/DAO/referentiels/frequentations/FrequentationDAOImpl.java
+++ src/main/java/DAO/referentiels/frequentations/FrequentationDAOImpl.java
@@ -20,7 +20,7 @@ public class FrequentationDAOImpl<Frequentation> implements FrequentationDAO{
 
 
             try {
-                String sql = "INSERT INTO FrequentationMagasin (idFrequentation, idMagasin, idClient, dateEntree) values (null, (FLOOR(RAND()*25)), (FLOOR(RAND()*25)), null);";
+                String sql = "INSERT INTO FrequentationMagasin (idFrequentation, idMagasin, idClient, dateEntree) values (null, (FLOOR(RAND()*25)), (FLOOR(RAND()*25)), (SELECT FROM_UNIXTIME(RAND() * (UNIX_TIMESTAMP() - UNIX_TIMESTAMP('2019-03-15')) + UNIX_TIMESTAMP('2019-03-15'))));";
                 System.out.println(sql);
                 st = connect.prepareStatement(sql);
                 st.executeUpdate();
